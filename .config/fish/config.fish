1834ef7559fb1db570a3a4932d15f1818f56d267
Bastien Duplessier <bastien.duplessier@gmail.com>
2017-02-15 23:09:09 +0100

    cleaning config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,23 +1,19 @@
 # Aliases
-alias emacs='emacs -nw'
 alias em='emacs -nw'
 alias la='ls -la'
 alias ll='ls -l'
 alias grer='grep -r'
 
 # Rails aliases
-alias be='bundle exec'
-alias brake='be rake'
-alias r='rails'
-alias rc='r c'
-alias rs='r s'
+alias bx='bundle exec'
+alias brake='bx rake'
 
 # Use terminal 256color
 set TERM xterm-256color
 
 # Use RBenv
 set PATH $PATH ~/.rbenv/bin
-. (rbenv init -|psub)
+status --is-interactive and . (rbenv init -|psub)
 
 # Use Haxe
 set PATH $PATH /usr/lib/haxe
@@ -26,12 +22,13 @@ set PATH $PATH /usr/lib/haxe
 set PATH $PATH /usr/bin/kerl
 set PATH $PATH ~/.rebar3
 
-# Don't use Vim
+# Don't use Vim meh
 alias vi='em'
 alias vim='em'
 alias vim.tiny='em'
 
-status --is-interactive and . (rbenv init -|psub)
-
 # OPAM configuration
 # source /home/bastien/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
+
+# Erlang
+. /home/$username/erlang/versions/19.2/activate.fish
