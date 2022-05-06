1e8a6b459d79b8ff0367be989c7256a2975017c6
Bastien Duplessier <bastien.duplessier@shipfix.com>
2022-05-06 16:59:23 +0200

    update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,37 +1,21 @@
-# Switch Caps Lock & Left Ctrl
-setxkbmap -option ctrl:swapcaps
+## GTK Fix
+set -gx GDK_CORE_DEVICE_EVENTS 1
 
-# Aliases
+## Prompt
+starship init fish | source
+
+## ASDF
+source /opt/asdf-vm/asdf.fish
+
+## Daily
+alias dc="docker-compose"
 alias em='emacs -nw'
 alias la='ls -la'
 alias ll='ls -l'
 alias grer='grep -r'
-
-# Rails aliases
-alias bx='bundle exec'
-alias brake='bx rake'
-
-# Use terminal 256color
-set TERM xterm-256color
-
-# Use RBenv
-set PATH $PATH ~/.rbenv/bin
-status --is-interactive and . (rbenv init -|psub)
-
-# Use Haxe
-set PATH $PATH /usr/lib/haxe
-
-# Use Erlang
-set PATH $PATH /usr/bin/kerl
-set PATH $PATH ~/.rebar3
+alias tailf='tail -f'
 
 # Don't use Vim meh
 alias vi='em'
 alias vim='em'
 alias vim.tiny='em'
-
-# OPAM configuration
-# source /home/bastien/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
-
-# Erlang
-. /home/$username/erlang/versions/19.2/activate.fish
