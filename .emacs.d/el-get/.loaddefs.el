0eb55def383ea57f31e65bba8e4c418585668c22
Bastien Duplessier <bastien.duplessier@shipfix.com>
2022-05-16 14:54:43 +0200

    remove .emacs.d

--- .emacs.d/el-get/.loaddefs.el
+++
@@ -1,21 +0,0 @@
-;;; .loaddefs.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("company-web/company-web.el" "dash/dash-functional.el"
-;;;;;;  "dash/dash.el" "el-get/el-get-autoloading.el" "el-get/el-get-build.el"
-;;;;;;  "el-get/el-get-byte-compile.el" "el-get/el-get-core.el" "el-get/el-get-custom.el"
-;;;;;;  "el-get/el-get-dependencies.el" "el-get/el-get-install.el"
-;;;;;;  "el-get/el-get-methods.el" "el-get/el-get-notify.el" "el-get/el-get-recipes.el"
-;;;;;;  "el-get/el-get-status.el") (22423 44379 9259 454000))
-
-;;;***
-
-(provide '.loaddefs)
-;; Local Variables:
-;; version-control: never
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; .loaddefs.el ends here
--- .emacs.d/elpa/json-reformat-20151007.1728/json-reformat-autoloads.el
+++
@@ -1,31 +0,0 @@
-;;; json-reformat-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "json-reformat" "json-reformat.el" (22102 50062
-;;;;;;  116052 890000))
-;;; Generated autoloads from json-reformat.el
-
-(autoload 'json-reformat-region "json-reformat" "\
-Reformat the JSON in the specified region.
-
-If you want to customize the reformat style,
-please see the documentation of `json-reformat:indent-width'
-and `json-reformat:pretty-string?'.
-
-\(fn BEGIN END)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("json-reformat-pkg.el") (22102 50062 175900
-;;;;;;  893000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; json-reformat-autoloads.el ends here
